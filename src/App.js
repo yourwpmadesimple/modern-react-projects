bb5a46cb62a31ecf33b61ae1953c922b22532452
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-25 14:24:40 -0500

    imported TodoList to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
-import './App.css'
+import React from "react";
+import { hot } from "react-hot-loader";
+
+import TodoList from "./todos/todolist/TodoList";
+
+import "./App.css";
 
 const App = () => (
-    <div className="App">
-        <h1>Hello, World!!</h1>
-    </div>
-)
+  <div className="App">
+    <TodoList />
+  </div>
+);
 
-export default hot(module)(App)
+export default hot(module)(App);
