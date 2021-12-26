80382310ecdf72243813225987ca3fdd52da10b7
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-26 08:24:57 -0500

    initial commit, Lesson 8 branch

---
+++ src/redux/actions.jsx
@@ -0,0 +1,15 @@
+// Define Redux Actions
+
+// Todo Action that fires when user types into the NewForm Component
+
+export const CREATE_TODO = "CREATE_TODO";
+export const createTodo = (text) => ({
+  type: CREATE_TODO,
+  payload: { text },
+});
+
+export const REMOVE_TODO = "REMOVE_TODO";
+export const removeTodo = (text) => ({
+  type: REMOVE_TODO,
+  payload: { text },
+});
