f476b3be6082f6e95e85606d180dd444a454b413
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-26 20:06:40 -0500

    mods to thunks.jsx, and actions.jsx

--- src/assets/thunks.jsx
+++ src/assets/thunks.jsx
@@ -1,3 +1,17 @@
+import {
+  loadTodosInProgress,
+  loadTodosSuccess,
+  loadTodosFailure,
+} from "../redux/actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+  dispatch(loadTodosInProgress());
+  const response = await fetch("http://localhost:8080/todos");
+  const todos = await response.json();
+
+  dispatch(loadTodosSuccess(todos));
+};
+
 export const displayAlert = (text) => () => {
   alert(`You clicked ${text}`);
 };
