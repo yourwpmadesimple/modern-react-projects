0d65fda0f56869cb68fc35eb79c439e19ebf320a
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-26 20:12:02 -0500

    thunk.jsx mods

--- src/assets/thunks.jsx
+++ src/assets/thunks.jsx
@@ -5,13 +5,18 @@ import {
 } from "../redux/actions";
 
 export const loadTodos = () => async (dispatch, getState) => {
-  dispatch(loadTodosInProgress());
-  const response = await fetch("http://localhost:8080/todos");
-  const todos = await response.json();
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch("http://localhost:8080/todos");
+    const todos = await response.json();
 
-  dispatch(loadTodosSuccess(todos));
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
 };
 
 export const displayAlert = (text) => () => {
-  alert(`You clicked ${text}`);
+  alert(`${text}`);
 };
